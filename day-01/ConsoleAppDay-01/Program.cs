18e00740986b49062975aceeab77f20ec2f079b5
Lukas Bugaj <lukas.bugaj@cas.de>
2018-12-01 13:47:22 +0100

    --- Day 1: Chronal Calibration ---
    Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?

---
+++ day-01/ConsoleAppDay-01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleAppDay_01
+{
+    class Program
+    {
+        private static readonly HashSet<int> reachedFrequencies = new HashSet<int>();
+        static void Main(string[] args)
+        {
+            PuzzleOne();
+            PuzzeTwo();
+        }
+
+        private static void PuzzleOne()
+        {
+            Console.WriteLine("==== Puzzle 1 ==== ");
+            Console.Write("Starting frequency: ");
+            var startingFrequency = new List<string> { Console.ReadLine() };
+            var sumOfChanges =   startingFrequency
+                .Concat(File.ReadLines("input.txt"))
+                .Select(int.Parse)
+                .Sum();
+            Console.WriteLine("resulting frequency: {0}", sumOfChanges);
+            Console.ReadKey();
+        }
+
+        private static void PuzzeTwo()
+        {
+            Console.WriteLine("==== Puzzle 2 ==== ");
+            Console.Write("Starting frequency: ");
+            var startingFrequency = new List<string> { Console.ReadLine() };
+            try
+            {
+                var aggregateOfChanges = startingFrequency
+                    .Concat(ReadInputIndefinitely())
+                    .Select(int.Parse)
+                    .Aggregate(AggregateFrequencChangeAndCheckForDuplicateResultungFreq);
+                Console.WriteLine("resulting frequency: {0}", aggregateOfChanges);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private static int AggregateFrequencChangeAndCheckForDuplicateResultungFreq(int currentFrequency, int frequencyChange)
+        {
+            var resultingFrequency = currentFrequency + frequencyChange;
+            if (reachedFrequencies.Contains(resultingFrequency))
+            {
+                throw new Exception("First frequency reached twice is: " + resultingFrequency);
+            }
+
+            reachedFrequencies.Add(resultingFrequency);
+            return resultingFrequency;
+        }
+
+        private static IEnumerable<string> ReadInputIndefinitely()
+        {
+            while (true)
+            {
+                foreach (var line in File.ReadLines("input.txt"))
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
